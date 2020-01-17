73dac079dad85de9a45545f2252eba64c5e601f9
cntanglijun <869058216@qq.com>
2020-01-17 19:32:55 +0800

    chore: update e2e script

--- react-multi-page/test/e2e/integration/Index.spec.js
+++ react-multi-page/test/e2e/integration/Index.spec.js
@@ -1,5 +1,8 @@
 describe('The Home Page', function() {
   it('successfully loads', function() {
     cy.visit('/')
+    cy.contains('Index')
+    cy.contains('Link To Home').click()
+    cy.url().should('include', 'home')
   })
 })
